84218d229864406cfd1bda0ade51c9231e4baf68
Jana Stinn <jana-stinn@gmx.de>
2023-03-30 15:43:31 +0200

    test img

--- src/components/home/SplitContainer.tsx
+++ src/components/home/SplitContainer.tsx
@@ -150,7 +150,7 @@ export default function SplitContainer({ data }: Props) {
           }
 
           .inner-container {
-            display: block;
+            display: grid;
           }
 
           .image-wrapper {
