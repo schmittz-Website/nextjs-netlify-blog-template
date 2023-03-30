bcb76be484436a337b13e896678edea7c5754b72
Jana Stinn <jana-stinn@gmx.de>
2023-03-31 00:39:27 +0200

    impressum

---
+++ src/lib/impressum.ts
@@ -0,0 +1,8 @@
+import impressum from "../../impressum.json";
+
+type Impressum = {
+  readonly title: string;
+  readonly text: string;
+};
+
+export default impressum as Impressum;
--- src/pages/impressum.tsx
+++ src/pages/impressum.tsx
@@ -1,14 +1,40 @@
 import Layout from "../components/Layout";
+import impressum from "../lib/impressum";
+import Arrow from "../assets/arrow.svg";
 
 export default function Impressum() {
   return (
     <Layout>
-      Impressum
-      {/* {HomeSections} */}
+      <a href={'/'} className={'back'}>
+        <figure><Arrow /></figure>
+        <p>Home</p>
+      </a>
+      <h1>{impressum.title}</h1>
+      <div>{impressum.text}</div>
       <style jsx>{`
-
-        @media (min-width: 769px) {
-
+        div {
+          white-space: break-spaces;
+          padding: 50px;
+          max-width: 1000px;
+          margin: auto;
+          text-align: left;
+        }
+        .back {
+          text-align: left;
+          display: flex;
+          align-items: center;
+          margin: 50px 50px 0;
+          column-gap: 25px;
+        }
+        .back figure {
+          margin: 0;
+        }
+        .back p {
+          height: 28px;
+          color: var(--orange);
+          font-size: 1.25rem;
+          line-height: 1;
+          font-family: 'Playfair Display', serif;
         }
       `}</style>
     </Layout>
